515a8dbcf03074046b12d51c17b20bdea8b822a5
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2025-03-22 11:27:17 +0100

    Make home-direct-symlink-service-type extensible

--- azaostro/home/services.scm
+++ azaostro/home/services.scm
@@ -1,5 +1,6 @@
 (define-module (azaostro home services)
   #:use-module (ice-9 ftw)
+  #:use-module (srfi srfi-1)
   #:use-module (guix gexp)
   #:use-module (guix utils)
   #:use-module (gnu services)
@@ -31,13 +32,16 @@
      originals-dir)
     (cons dirs links)))
 
-
 (define-syntax path-to-local
   (syntax-rules ()
     ((path-to-local path)
      (string-append (current-source-directory) "/" path))))
 
-(define (home-direct-symlink-files original)
+(define (symlink-all-files-in-dir original)
+  (unless (string? original)
+    (error 'wrong-type
+           "home-direct-symlink-service-type: ORIGINAL must be a string: "
+           original))
   (let ((orig-dir-name (basename original)))
     (unless (string-prefix? "/" original)
       (error 'wront-path
@@ -50,34 +54,39 @@
     (let* ((dirs-links (create-direct-symlink-plan original))
            (dirs (car dirs-links))
            (links (cdr dirs-links)))
-      `((,orig-dir-name
-         ,(computed-file
-           orig-dir-name
-           #~(begin
-               (define (make-dirs dirs)
-                 (for-each (lambda (dir)
-                             (let ((full-dir (string-append #$output "/" dir)))
-                               (format #t "Creating directory ~a\n" full-dir)
-                               (mkdir full-dir)))
-                           dirs))
-               (define (make-symlinks links)
-                 (for-each (lambda (link)
-                             (let ((orig (car link))
-                                   (linkpath (string-append #$output "/" (cdr link))))
-                               (format #t "Symlinking ~a -> ~a\n" orig linkpath)
-                               (symlink orig linkpath)))
-                           links))
-               (mkdir #$output)
-               (make-dirs '#$dirs)
-               (make-symlinks '#$links))))))))
+      (list orig-dir-name
+            (computed-file
+             orig-dir-name
+             #~(begin
+                 (define (make-dirs dirs)
+                   (for-each (lambda (dir)
+                               (let ((full-dir (string-append #$output "/" dir)))
+                                 (format #t "Creating directory ~a\n" full-dir)
+                                 (mkdir full-dir)))
+                             dirs))
+                 (define (make-symlinks links)
+                   (for-each (lambda (link)
+                               (let ((orig (car link))
+                                     (linkpath (string-append #$output "/" (cdr link))))
+                                 (format #t "Symlinking ~a -> ~a\n" orig linkpath)
+                                 (symlink orig linkpath)))
+                             links))
+                 (mkdir #$output)
+                 (make-dirs '#$dirs)
+                 (make-symlinks '#$links)))))))
 
-;; TODO: make it extensible to enable multipe instances with "simple-service"
+(define (symlink-all-files-in-all-dirs orig-dirs)
+  (map symlink-all-files-in-dir orig-dirs))
+
 (define home-direct-symlink-service-type
   (service-type
    (name 'my-hello)
    (extensions
     (list (service-extension home-xdg-configuration-files-service-type
-                             home-direct-symlink-files)))
+                             symlink-all-files-in-all-dirs)))
+
+   (compose concatenate)
+   (extend (lambda (_ original-dir) original-dir))
    (description "Escape guix-home strict reproducibility and create
 a symlink in your .config directly to the source files rather than
 to a /gnu/store snapshot of it.  Provide the name of the directory
