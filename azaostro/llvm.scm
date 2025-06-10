b1cc01aca99c9249e8ffa86861de073419a7b771
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2025-06-10 11:22:01 +0200

    Upgrade clang to clang 20

--- azaostro/llvm.scm
+++ azaostro/llvm.scm
@@ -21,11 +21,26 @@
 ;;; which is expanded in the beginning of a compiler invocation.
 ;;; Hold off on using clang-19 as it is not yet supported by the work project z3
 ;;; contains a bug in template code that breaks clang-19 compilation.
+;;; This should be superceded by clang-with-lld-20
 (define-public clang-with-lld-18
   (package
     (inherit (package-with-configure-flags clang-18 #~(list "-DCLANG_DEFAULT_LINKER=lld")))
     (inputs (modify-inputs (package-inputs clang-18) (replace "gcc" "gcc-13")))
-    (name "clang-with-lld")))
+    (name "old-clang-with-lld")))
 
 (define-public clang-toolchain-with-lld-18
   (make-clang-toolchain clang-with-lld-18 libomp-18))
+
+;;; Use LLD as the default linker instead of BFD used by clang-20.
+;;; LLD has the advantage of being insensitive to the order in which
+;;; it has libraries listed, which is useful when compiling our work project
+;;; since it injects the home-built standard library to the CMAKE_CXX_FLAGS
+;;; which is expanded in the beginning of a compiler invocation.
+(define-public clang-with-lld-20
+  (package
+    (inherit (package-with-configure-flags clang-20 #~(list "-DCLANG_DEFAULT_LINKER=lld")))
+    (inputs (modify-inputs (package-inputs clang-20) (replace "gcc" "gcc-13")))
+    (name "clang-with-lld")))
+
+(define-public clang-toolchain-with-lld-20
+  (make-clang-toolchain clang-with-lld-20 libomp-20))
